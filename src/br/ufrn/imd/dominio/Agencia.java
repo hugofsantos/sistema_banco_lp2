b68edfc09dc74f3dd0bc25a9b07c402f8a7ddf88
Hugo Santos <hf.santos2332@gmail.com>
2022-09-18 15:00:49 -0300

    Implementa classe Agencia

--- src/br/ufrn/imd/dominio/Agencia.java
+++ src/br/ufrn/imd/dominio/Agencia.java
@@ -1,5 +1,43 @@
 package br.ufrn.imd.dominio;
 
-public class Agencia {
+import java.util.Objects;
 
+public class Agencia {
+    private int codigo;
+    private Banco banco;
+
+    /**
+     * Criar um objeto agência
+     * @param codigo da agência
+     * @param banco da agência
+     */
+    public Agencia(int codigo, Banco banco){
+        this.codigo = codigo;
+        this.banco = banco;
+    }
+
+    /**
+     * Obter o código da agência
+     * @return código da agência
+     */
+    public int getCodigo(){
+        return this.codigo;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(this.codigo);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agencia other = (Agencia) obj;
+		return (this.codigo == other.codigo) && (this.banco == other.banco);
+    }    
 }
